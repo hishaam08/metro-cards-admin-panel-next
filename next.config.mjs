5fe0fb1b05aa9c49d627e734ef44290743d2fee8
Mohamed Hishaam <89605014+hishaam08@users.noreply.github.com>
2024-05-31 18:12:23 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   env: {
-    DB_LOCAL_URI:
-      "mongodb+srv://Hishaam:hishaam%40mongodb@nodeprojects.fwck6dz.mongodb.net/METRO-CARDS?retryWrites=true&w=majority",
+
   },
   images: {
     remotePatterns: [
